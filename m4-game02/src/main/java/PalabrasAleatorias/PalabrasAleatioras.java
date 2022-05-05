fce29ad6611216d5aa2d79030e9e9fdde7d73e9c
Daniel Fernandez Cacho <Dani.Fernandezcacho@gmail.com>
2022-05-05 12:39:38 +0200

    Palabras Aleatorias listo

---
+++ m4-game02/src/main/java/PalabrasAleatorias/PalabrasAleatioras.java
@@ -0,0 +1,17 @@
+package PalabrasAleatorias;
+
+import java.util.Random;
+
+public class PalabrasAleatioras {
+
+	public String GenerarPalabra () {
+		 String[] x = {"perro", "gato", "hola", "bien", "programar", "palabra", "azar","otra","casa","frio"};
+	        Random rand = new Random();
+
+	        int n = rand.nextInt(10);
+	        String z = x [n];
+	        
+	        return z;
+	        
+	}
+}
